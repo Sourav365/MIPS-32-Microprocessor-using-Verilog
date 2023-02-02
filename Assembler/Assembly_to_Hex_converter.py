2e8aec43b5888d0f9a79475c6e5473f882833f9a
Sourav Das <49667585+Sourav365@users.noreply.github.com>
2023-02-02 19:47:33 +0530

    Uses input assembly file

--- Assembler/Assembly_to_Hex_converter.py
+++ Assembler/Assembly_to_Hex_converter.py
@@ -1,7 +1,7 @@
-file_in = open('F:\\Xilinx_Vivado\\011MIPS32_processor\\instructions\\assembly.txt','r')
+file_in = open('F:\\Xilinx_Vivado\\011MIPS32_processor\\instructions\\test2_data.txt','r')
 lines = file_in.readlines()
 
-file_out = open("F:\\Xilinx_Vivado\\011MIPS32_processor\\instructions\\hex_code.txt", "w")
+file_out = open("F:\\Xilinx_Vivado\\011MIPS32_processor\\instructions\\hex_code_new.txt", "w")
 
 ADD ='000000'; SUB ='000001'; AND  ='000010';
 OR  ='000011'; SLT ='000100'; MUL  ='000101';
@@ -17,50 +17,55 @@ for line in lines:
     y=""
     z=""
     encoded="";
-    match(x[0]):
-        case 'ADD'  :
-            y=(x[1]).split(',');
+    
+    match(x[1]):
+        case 'ADD'  :       ##ADD rd,rs,rt => OPCODE | rs | rt | rd | 11{0}
+            y=(x[2]).split(',');
             encoded = ADD + '{:05b}'.format(int(y[1][1:])) +\
             '{:05b}'.format(int(y[2][1:])) + '{:05b}'.format(int(y[0][1:])) + 11*'0';             
 
-        case 'SUB'  :
-            y=(x[1]).split(',');
+        case 'SUB'  :       ##SUB rd,rs,rt => OPCODE | rs | rt | rd | 11{0}
+            y=(x[2]).split(',');
             encoded = SUB + '{:05b}'.format(int(y[1][1:])) +\
             '{:05b}'.format(int(y[2][1:])) + '{:05b}'.format(int(y[0][1:])) + 11*'0';               
 
-        case 'AND'  :
-            y=(x[1]).split(',');
+        case 'AND'  :       ##AND rd,rs,rt => OPCODE | rs | rt | rd | 11{0}
+            y=(x[2]).split(',');
             encoded = AND + '{:05b}'.format(int(y[1][1:])) +\
             '{:05b}'.format(int(y[2][1:])) + '{:05b}'.format(int(y[0][1:])) + 11*'0';
 
-        case 'OR'   :
-            y=(x[1]).split(',');
+        case 'OR'   :       ##OR  rd,rs,rt => OPCODE | rs | rt | rd | 11{0}
+            y=(x[2]).split(',');
             encoded = OR  + '{:05b}'.format(int(y[1][1:])) +\
             '{:05b}'.format(int(y[2][1:])) + '{:05b}'.format(int(y[0][1:])) + 11*'0';
 
-        case 'SLT'  :
-            encoded = SLT
+        case 'SLT'  :       ##SLT rd,rs,rt => OPCODE | rs | rt | rd | 11{0}
+            y=(x[2]).split(',');
+            encoded = SLT  + '{:05b}'.format(int(y[1][1:])) +\
+            '{:05b}'.format(int(y[2][1:])) + '{:05b}'.format(int(y[0][1:])) + 11*'0';
 
-        case 'MUL'  :
-            encoded = MUL
+        case 'MUL'  :       ##MUL rd,rs,rt => OPCODE | rs | rt | rd | 11{0}
+            y=(x[2]).split(',');
+            encoded = MUL  + '{:05b}'.format(int(y[1][1:])) +\
+            '{:05b}'.format(int(y[2][1:])) + '{:05b}'.format(int(y[0][1:])) + 11*'0';
 
-        case 'LW'   :
-            y=(x[1]).split(','); z=y[1].split('(');
+        case 'LW'   :       ##LW rt,Imm_data(rs) => OPCODE | rs | rt | Imm_data
+            y=(x[2]).split(','); z=y[1].split('(');
             encoded = LW + '{:05b}'.format(int(z[1][1:-1])) +\
             '{:05b}'.format(int(y[0][1:])) + '{:016b}'.format(int(z[0]))
 
-        case 'SW'   :
-            y=(x[1]).split(','); z=y[1].split('(');
-            encoded = SW + '{:05b}'.format(int(y[0][1:])) +\
-            '{:05b}'.format(int(z[1][1:-1])) + '{:016b}'.format(int(z[0]))
+        case 'SW'   :       ##SW rs,Imm_data(rt) => OPCODE | rt | rs | Imm_data
+            y=(x[2]).split(','); z=y[1].split('(');
+            encoded = SW + '{:05b}'.format(int(z[1][1:-1])) +\
+            '{:05b}'.format(int(y[0][1:])) + '{:016b}'.format(int(z[0]))
         
-        case 'ADDI' :
-            y=(x[1]).split(',');
+        case 'ADDI' :       ##ADDI rt,rs,Imm_data => OPCODE | rs | rt | Imm_data
+            y=(x[2]).split(',');
             encoded = ADDI + '{:05b}'.format(int(y[1][1:])) +\
             '{:05b}'.format(int(y[0][1:])) + '{:016b}'.format(int(y[2]));               
 
-        case 'SUBI' :
-            y=(x[1]).split(',');
+        case 'SUBI' :       ##SUBI rt,rs,Imm_data => OPCODE | rs | rt | Imm_data
+            y=(x[2]).split(',');
             encoded = SUBI + '{:05b}'.format(int(y[1][1:])) +\
             '{:05b}'.format(int(y[0][1:])) + '{:016b}'.format(int(y[2]));
                     
@@ -80,8 +85,8 @@ for line in lines:
 
     out = '{:08x}'.format(int(encoded,2)) #32-bit hex_string(8)
     print(out)
-    file_out.write(out)
-    file_out.write('\n')
+    file_out.writelines(x[0]+'  '+out+'\t//'+' '.join(x[1:])+'\n')
+
 
 file_in.close()
 file_out.close()
